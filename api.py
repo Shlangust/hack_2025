c9818c02b0249e9edbab983bc19bcbdf1217d0b4
Shlangust <SlusarevER@gmail.com>
2025-04-05 12:21:33 +0300

    for my n

---
+++ api.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, UploadFile, File
+# from main import read
+# import io
+from typing import Annotated
+
+app = FastAPI()
+# import polars as pl
+
+@app.post("/upload_excel")
+async def create_file(file: Annotated[bytes | None, File()] = None):
+    print(file)
+    # if not file:
+    #     return {"message": "No file sent"}
+    # else:
+    #     return {"file_size": len(file)}
+    return 0
--- main.py
+++ main.py
@@ -1,30 +1,56 @@
+import time
+import io
+
 import polars as pl
+import os
+possible_values = ['Дата', 'День', 'Число']
 
 def read(table,format):
-    df = pl.read_excel(table)
-    if format == 'hor':
+
+        # df = pl.read_excel(table)
+        table_stream = io.BytesIO(table)
+
+
+        df = pl.read_excel(table_stream)
         a = df[df.columns[0]].to_list()
-        strt = a.index('Дата')+2
+        strt = 0
+        val = ''
 
+        for value in possible_values:
+                if value in a:
+                        strt = a.index(value)
+                        val = value
+                        break
+                elif value == df.columns[0]:
+                        strt = 0
+                        val = value
+                        break
         df = df.slice(strt, df.height - strt)
-        print(df)
-        pandas_df = df.to_pandas()
+        df_clean = df.slice(2)
 
-        json_data = pandas_df.to_dict(orient="records")
+        new_columns = [val] + [str(i) for i in range(24)]
+        df_clean = df_clean.rename({old: new for old, new in zip(df_clean.columns, new_columns)})
+        # df_clean = df_clean.with_columns([
+        #         pl.when(pl.col(val).str.contains('-'))  # проверяем, есть ли дефис (часть из возможных форматов)
+        #         .then(pl.col(val).str.to_datetime(format="%Y-%m-%d %H:%M:%S", strict=False))  # формат 1
+        #         .otherwise(pl.col(val).str.to_datetime(format="%d/%m/%Y %H:%M:%S", strict=False))
+        #         .cast(pl.Date)  # затем приводим к типу Date
+        # ])
+
+        json_data = df_clean.to_dicts()
 
         return json_data
-    elif format== 'ver':
-        print(df)
-        a = df[df.columns[0]].to_list()
-        strt = a.index('Дата') + 2
-        df = df.slice(strt, df.height - strt)
-        print('\n\n\n\n\n\n')
-        print(df)
-        pandas_df = df.to_pandas()
 
-        json_data = pandas_df.to_dict(orient="records")
 
-        return json_data
-# print(read('data.xlsx','hor')[0])
-# print(read('data1.xlsx','ver')[0])
-print(read('data3.xlsx','hor'))
+# folder_path = 'C:/hack'
+
+# Получение списка файлов
+# files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+# start_time = time.time()
+# a = read(files[0],'hor')
+file_path = "ver.xlsx"
+
+# Открываем файл в бинарном режиме и получаем байты
+with open(file_path, "rb") as file:
+    file_bytes = file.read()
+# print(read(file_bytes,'ver'))
